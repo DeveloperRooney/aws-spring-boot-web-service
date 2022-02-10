1e37b9a74644403b75d9165eee26f217175f060c
DeveloperRooney <han_0427@naver.com>
2022-02-10 15:34:29 +0900

    게시글 업데이트 API 테스트 코드 작성

--- src/test/java/com/aws/rooney/web/ArticleApiControllerTest.java
+++ src/test/java/com/aws/rooney/web/ArticleApiControllerTest.java
@@ -3,6 +3,7 @@ package com.aws.rooney.web;
 import com.aws.rooney.domain.article.Article;
 import com.aws.rooney.domain.article.ArticleRepository;
 import com.aws.rooney.web.dto.ArticleSaveRequestDto;
+import com.aws.rooney.web.dto.ArticleUpdateRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -59,6 +62,44 @@ public class ArticleApiControllerTest {
         List<Article> articleList = articleRepository.findAll();
         assertThat(articleList.get(0).getTitle()).isEqualTo(title);
         assertThat(articleList.get(0).getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void update() throws Exception{
+
+        // given
+        Article saveArticle = articleRepository.save(Article.builder()
+                .title("title")
+                .content("content")
+                .author("author").build());
+
+        Long updateArticleId = saveArticle.getId();
+        String nextTitle = "title2";
+        String nextContent = "content2";
+
+        ArticleUpdateRequestDto requestDto = ArticleUpdateRequestDto.builder()
+                .title(nextTitle)
+                .content(nextContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/article/" + updateArticleId;
+
+        HttpEntity<ArticleUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.
+                exchange(url, HttpMethod.PUT,
+                requestEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Article> articleList = articleRepository.findAll();
+        assertThat(articleList.get(0).getTitle()).isEqualTo(nextTitle);
+        assertThat(articleList.get(0).getContent()).isEqualTo(nextContent);
 
     }
 }
